1f1663555f668e6bdfea95d519c92c7ae701f5d6
Lex <lexd89@gmail.com>
2016-11-10 16:52:58 +0100

    azure commit from windows

--- helloworld.go
+++ helloworld.go
@@ -9,7 +9,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hi, RageSquid!")
 }
 
+func PrintLine() {
+	fmt.Println("hello thur")
+}
+
 func main() {
-    http.HandleFunc("/ragesquid", handler)
+	PrintLine()
+    http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
 }
